0ab137fc663c17451a7160c9ee586a1e1330fc1a
Marcin Pytlak <marcin.pytlak@o2.pl>
2021-04-03 20:55:15 +0200

    add waiter and login view

--- src/components/views/Login/Login.js
+++ src/components/views/Login/Login.js
@@ -1,11 +1,60 @@
 import React from 'redux';
 // import PropTypes from 'prop-types';
 import styles from './Login.module.scss';
+import Avatar from '@material-ui/core/Avatar';
+import Container from '@material-ui/core/Container';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import Typography from '@material-ui/core/Typography';
+
 
 const Login = () => (
-  <div className={styles.component}>
-    <h2>Login view</h2>
-  </div>
+  <Container component="main" maxWidth='xs'>
+    <CssBaseline />
+    <div className={styles.paper}>
+      <Avatar>
+        <LockOutlinedIcon />
+      </Avatar>
+      <Typography component="h1" variant="h5">
+          Sign in
+      </Typography>
+      <form className={styles.form} noValidate>
+        <TextField
+          fullWidth
+          required
+          variant="standard"
+          name="email"
+          id="email"
+          label="Email Address"
+          autoComplete="email"
+          autoFocus
+          margin="normal"
+        />
+        <TextField
+          fullWidth
+          required
+          variant="standard"
+          name="password"
+          id="password"
+          label="Password"
+          autoComplete="password"
+          autoFocus
+          margin="normal"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="primary"
+          href={process.env.PUBLIC_URL + '/'}
+        >
+            Sign In
+        </Button>
+      </form>
+    </div>
+  </Container>
 );
 
 export default Login;
--- src/components/views/Waiter/Waiter.js
+++ src/components/views/Waiter/Waiter.js
@@ -4,6 +4,23 @@ import styles from './Waiter.module.scss';
 import { Link } from 'react-router-dom';
 import Breadcrumbs from '@material-ui/core/Breadcrumbs';
 import Button from '@material-ui/core/Button';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+
+const createData = (table, status, activityTime, deletion, add) => {
+  return {table, status, activityTime, deletion, add};
+};
+
+const rows = [
+  createData('1', 'status', '40 min ago', <Button size="small" variant="outlined" color="secondary">Delete</Button>, <Button size="small" variant="contained" color="primary">Add</Button> ),
+  createData('2', 'status', '31 min ago', <Button size="small" variant="outlined" color="secondary">Delete</Button>, <Button size="small"  variant="contained"color="primary">Add</Button> ),
+  createData('3', 'status', '42 min ago', <Button size="small" variant="outlined" color="secondary">Delete</Button>, <Button size="small"  variant="contained"color="primary">Add</Button> ),
+];
 
 const Waiter = () => (
   <div className={styles.component}>
@@ -12,6 +29,30 @@ const Waiter = () => (
       <Button component={Link} to="waiter/order/:id">Waiter order id</Button>
     </Breadcrumbs>
     <h2>Waiter view</h2>
+    <TableContainer component={Paper}>
+      <Table>
+        <TableHead>
+          <TableRow>
+            <TableCell>Table</TableCell>
+            <TableCell align="right">Table Status</TableCell>
+            <TableCell align="right">Last activity</TableCell>
+            <TableCell align="right">Actions</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.table}>
+              <TableCell component="th" scope="row">
+                {row.table}
+              </TableCell>
+              <TableCell align="right">{row.status}</TableCell>
+              <TableCell align="right">{row.activityTime}</TableCell>
+              <TableCell align="right">{row.deletion}{row.add}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table> 
+    </TableContainer>
   </div>
 );
 
